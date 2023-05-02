3f884fa87fdf4281235bfb23e7aa65beba6265e1
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-05-02 16:54:14 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,73 +1,59 @@
-//PASSO 1 CRIAR AS VARIÁVEIS
+//declarando as variáveis..
 var trex_correndo, trex;
 var solo, soloImagem, soloInvisivel;
-var nuvem, nuvemImagem;
-
-//como há 6 imagens de cacto, logo há 6 variáveis para guardá-las
-
-//CARREGAR ARQUIVOS DE MÍDIA
+//carregando os arquivos de mídia
 function preload() {
     soloImagem = loadImage("solo.png");
-    nuvemImagem = loadImage("nuvem.png");
-    //esses códigos guardam as imagens dos cactos nas variáveis
-    //dessa maneira, elas ficam salvas na memória do programa
- 
-
-
+    nuvemImg = loadImage("nuvem.png");
     trex_correndo = loadAnimation("trex1.png", "trex2.png", "trex3.png");
 }
 
 function setup() {
     createCanvas(600, 200);
-    //trex
-    trex = createSprite(50, 180, 50, 50);
-    trex.addAnimation("correndo", trex_correndo);
-    trex.scale = 0.5;
 
-    //solo
+    //criando o solo...    
     solo = createSprite(300, 190, 600, 20);
+    
+    soloInvisivel = createSprite(300, 200, 600, 20);
+    soloInvisivel.visible = false;
     solo.addImage(soloImagem);
     solo.velocityX = -3;
 
-    soloInvisivel = createSprite(300, 199, 600, 2);
-    soloInvisivel.visible = false;
+    //criando o trex..
+    trex = createSprite(50, 180, 50, 50);
+    trex.addAnimation("correndo", trex_correndo);
+    trex.scale = 0.5;  
+
+
 }
 
 function draw() {
-    background("black");
-
+    background("white");
+    
     if (solo.x < 0) {
         solo.x = solo.width / 1.99;
     }
 
-    if (keyDown("space") && trex.y > 140) {
+    if (keyDown("space") && trex.isTouching(solo)) {
         trex.velocityY = -10;
     }
 
-    trex.velocityY += 0.8;
-
+    trex.velocityY += 0.8
     trex.collide(soloInvisivel);
-
-    gerarNuvens();
-  
     drawSprites();
+    //chama a função
+    criarNuvens();
 
+ 
 }
 
-function gerarNuvens() {
-
-    if (frameCount % 60 == 0) {
-        var y = Math.round(random(1, 100));
-        var nuvem = createSprite(600, y, 50, 20);
-        nuvem.addImage(nuvemImagem);
-        nuvem.velocityX = -3;
-        //o tamanho da nuvem é 0.5
-        
-        //a nuvem permanece na tela por 206 quadros
-       
-        //a profundidade do trex é mais alta que a da nuvem
-       
+function criarNuvens(){
+    if(frameCount%250==0){
+        var nuvem = createSprite(600,Math.round(random(0, 100)));
+        nuvem.addImage(nuvemImg);
+        nuvem.velocityX = -3; 
+        //deixa o trex na frente da nuvem;
+        trex.depth = nuvem.depth + 1;
     }
 
 }
-//código para criar os cactos
